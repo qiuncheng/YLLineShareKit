5f77af964dcc54ccc6a3c5ae5720787ea433b1a4
vsccw <vsccw@QingChunMBP.lan>
2017-04-28 23:08:21 +0800

    Add project.

---
+++ YLLineShareKit/YLLineShareKit/YLLineShareKit.h
@@ -0,0 +1,19 @@
+//
+//  YLLineShareKit.h
+//  YLLineShareKit
+//
+//  Created by vsccw on 2017/4/28.
+//  Copyright © 2017年 Qiun Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLLineShareKit.
+FOUNDATION_EXPORT double YLLineShareKitVersionNumber;
+
+//! Project version string for YLLineShareKit.
+FOUNDATION_EXPORT const unsigned char YLLineShareKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLLineShareKit/PublicHeader.h>
+
+
